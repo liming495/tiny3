000a9d1a10604c6932d185fc18dd7a979f36e2c3
wop129@tom.com <wop129@tom.com>
2019-05-31 14:07:17 +0800

    init

---
+++ zipkin/src/main/java/com/guppy/zipkin/ZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.guppy.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+
+}
