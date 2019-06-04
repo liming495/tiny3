e2819137fb9c0a891f3d6c4a4d88d27f01823298
wop129@tom.com <wop129@tom.com>
2019-06-04 15:56:33 +0800

    modify

---
+++ auth-service/src/main/java/com/guppy/auth/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.guppy.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 重写AuthenticationManager
+ *
+ * @author Guppy
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
